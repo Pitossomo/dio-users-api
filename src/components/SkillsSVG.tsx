b94651d3e2f3f32210d12717e336c7e62ac3c9d8
pitossomo <pitossomo@gmail.com>
2023-02-23 23:07:32 -0300

    ✨feat: Make image from parsed skills' data

--- src/components/SkillsSVG.tsx
+++ src/components/SkillsSVG.tsx
@@ -5,19 +5,35 @@ type SkillSVGProps = {
 };
 
 export default function SkillsSVG({ skills }: SkillSVGProps) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg">
-      {skills.length > 0 ? (
-        skills.map((skill) => (
-          <>
-            <text>{skill.name}</text>
-            <text>{skill.exp}</text>
-            <image xlinkHref={skill.imgUrl}></image>
-          </>
-        ))
-      ) : (
-        <text>😢 No skill yet 😢</text>
-      )}
-    </svg>
-  );
+  return `
+    <svg xmlns="http://www.w3.org/2000/svg"
+      viewbox="0 0 300 300"
+      width="300" height="300"
+      style="background-color:black"
+    >
+      <style>
+        text { 
+          font: normal 8px monospace;
+          fill: white;
+        }
+      </style>
+      ${
+        skills.length > 0
+          ? `<text x="0" y="15">Skills</text>` +
+            skills
+              .map(
+                (skill, i) => `
+              <g>
+                <image x="150" y="${i * 30 + 15}" href="${
+                  skill.imgUrl
+                }" height="30" width="30" />
+                <text x="0" y="${(i + 1) * 30}">${skill.name}</text>
+                <text x="70" y="${(i + 1) * 30}">${skill.exp}</text>
+              </g>
+            `
+              )
+              .join()
+          : `<text>😢 No skill yet 😢</text>`
+      }
+    </svg>`;
 }
